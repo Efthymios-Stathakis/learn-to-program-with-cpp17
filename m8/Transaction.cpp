8aefe10e3f9e3bbfc843a078e505365a9afe2c09
Efthymios-Stathakis <efstatha@hotmail.com>
2024-09-20 14:43:26 +0200

    added cpp files for module 8

---
+++ m8/Transaction.cpp
@@ -0,0 +1,15 @@
+#include "Transaction.h"
+
+using std::string;
+using std::to_string;
+
+Transaction::Transaction(int amt, string kind) : amount(amt), type(kind)
+{
+}
+
+string Transaction::Report() const
+{
+    string report = " ";
+    report += type + " " + to_string(amount); 
+    return report;
+}
